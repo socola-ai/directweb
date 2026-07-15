8a3e4a156e3068637caa34756734e4bc648b29fa
SLDZPRO <sldzpro@example.com>
2026-07-15 22:10:42 +0700

    full

--- animation.js
+++ animation.js
@@ -12,7 +12,7 @@ const messages = [
 ];
 
 // ======= URL WEBSITE CHÍNH =======
-const TARGET_URL = "https://web-production-205d7.up.railway.app";
+const TARGET_URL = "https://web-production-3ff18.up.railway.app";
 // ================================
 
 const TOTAL_TIME = 12000; // 12 giây
